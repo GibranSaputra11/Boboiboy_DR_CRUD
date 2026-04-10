513e18eb9a7a5d55c508f559b31b5ab5e25fcb30
Gibran Saputra <gibranbantul@gmail.com>
2026-04-10 09:47:50 +0700

    add variable untuk koneksi ke database

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -13,6 +13,9 @@ namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=gibran-laptop;Initial Catalog=DBAkademiADO;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
