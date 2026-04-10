50c7d591360cd2b672e7b05853c2f4c6afaec004
Gibran Saputra <gibranbantul@gmail.com>
2026-04-10 09:40:06 +0700

    add library SqlClient

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,11 @@ namespace CRUDMahasiswaADO
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
