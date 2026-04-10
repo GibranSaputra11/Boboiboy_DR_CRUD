768edfc259e1c18953db9690e9431ba9805637ab
Gibran Saputra <gibranbantul@gmail.com>
2026-04-10 10:48:34 +0700

    membuat method klik [ada table terus akan mengembalikan dtanya ke form

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -156,6 +156,7 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(730, 253);
             this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btnConnect
             // 
